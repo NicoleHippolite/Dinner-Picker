4a14eff760cdf4f0bc5b41ecb6d9fa28a6cc3ae4
NicoleHippolite <nicky.elmo@gmail.com>
2016-08-06 14:01:48 +1200

    Add project files

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function hello(person) {
+    return "Hello, " + person;
+}
+var user = "What is up";
+hello(23823);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function hello(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "What is up";
+
+hello(23823); 
